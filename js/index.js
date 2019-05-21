ae618b4f270dbbd70db0096c5d9edceafdecd17a
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-05-21 18:40:23 -0400

    fix

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ mouseEvent.addEventListener("mouseover", function(event) {
 
 const Elan = document.querySelector("body");
 Elan.addEventListener('wheel', event => {
-    alert(event.textContent='Elan is the best-but dont be like Elan')
+    alert(event.textContent='Elan is the best *but dont be like Elan')
 });
 
 
@@ -33,3 +33,4 @@ function randomColor() {
 body.onclick = function() {
   body.style.backgroundColor = randomColor();
 };
+
