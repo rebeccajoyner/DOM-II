8df94d1f92a30f1fc4900ad4a29908368fa42817
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-05-21 18:20:04 -0400

    bodyclickcolor

--- js/index.js
+++ js/index.js
@@ -16,3 +16,20 @@ const wheel1 = document.querySelector("body");
 wheel1.addEventListener('wheel', event => {
     alert(event.textContent='Elan is the best-but dont be like Elan')
 });
+
+
+
+
+const body = document.querySelector('body');
+
+function random(number) {
+  return Math.floor(Math.random() * number);
+}
+
+function randomColor() {
+    return `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+}
+
+body.onclick = function() {
+  body.style.backgroundColor = randomColor();
+};
