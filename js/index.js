031b79f77755d5b187fec8ca6a46b9890068bad4
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-05-21 17:15:34 -0400

    mouseover1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+
+const mouseEvent = document.querySelector("nav");
+mouseEvent.addEventListener.addEventListener("mouseover", function (event) {
+    event.target.style.color = '#30f247';
+
+    setTimeout(function () {
+        event.target.style.color = "";
+    }, 400);
+}, false);
