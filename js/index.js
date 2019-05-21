efe29c0826e904d8c5b98bdc88669fca993638f7
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-05-21 17:56:05 -0400

    wheel

--- js/index.js
+++ js/index.js
@@ -11,3 +11,8 @@ mouseEvent.addEventListener("mouseover", function(event) {
 }, false);
 
 
+
+const wheel1 = document.querySelector("body");
+wheel1.addEventListener('wheel', event => {
+    alert(event.textContent='Elan is the best-but dont be like Elan')
+});
