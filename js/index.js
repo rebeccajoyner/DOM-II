67edf057bc058b99e195dd9a9d6ef6d9d88e06c8
rebeccajoyner <49732754+rebeccajoyner@users.noreply.github.com>
2019-07-08 17:41:44 -0400

    web21 new save

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ mouseEvent.addEventListener("mouseover", function(event) {
 
 const Elan = document.querySelector("body");
 Elan.addEventListener('wheel', event => {
-    alert(event.textContent='Elan is the best *but dont be like Elan')
+    alert(event.textContent='Most annoying popup.')
 });
 
 
